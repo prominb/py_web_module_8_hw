f7f1c22ac060fa9b342f05f8687123c898413601
Roman M <prominb@gmail.com>
2024-04-25 06:08:02 +0300

    edit Consumer e010 and Produser

---
+++ consumer.py
@@ -0,0 +1,42 @@
+import os
+import sys
+
+import pika
+
+from model_part2 import Contact
+import connect
+
+
+def main():
+    credentials = pika.PlainCredentials('guest', 'guest')
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=5672, credentials=credentials))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='sending_emails_queue')
+
+    def callback(ch, method, properties, body):
+        contact_id = body.decode()
+        contact = Contact.objects.with_id(contact_id)
+        if contact:
+            print(f"For ContactID: {contact_id}", end=" | ")
+            contact.update(set__ck_sent=True)
+            print(f"Message sent to {contact.email}.")
+        else:
+            print(f"Contact with ID {contact_id} not found.")
+
+
+    channel.basic_consume(queue='sending_emails_queue', on_message_callback=callback, auto_ack=True)
+
+    print('Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
--- producer.py
+++ producer.py
@@ -1,28 +1,33 @@
+from random import randint
+from bson.objectid import ObjectId
+
 import pika
 from faker import Faker
 
 from model_part2 import Contact
 import connect
 
+
 def main():
-    # credentials = pika.PlainCredentials('guest', 'guest')
-    # connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=5672, credentials=credentials))
-    # channel = connection.channel()
+    credentials = pika.PlainCredentials('guest', 'guest')
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=5672, credentials=credentials))
+    channel = connection.channel()
 
-    # channel.queue_declare(queue='sending_emails_queue')
+    channel.queue_declare(queue='sending_emails_queue')
 
+    number = randint(5, 10)
     fake = Faker()
-    def fill_data(number: int) -> None:
-        Contact.drop_collection()
-        for _ in range(number):
-            fullname = fake.name()
-            email = fake.email()
-            ck_sent = bool(0)
-            phone = fake.phone_number()
-            contact = Contact(fullname=fullname, email=email, ck_sent=ck_sent, phone=phone)            
-            contact.save()
-    
-    fill_data(3)
+    Contact.drop_collection()
+    for _ in range(number):
+        fullname = fake.name()
+        email = fake.email()
+        ck_sent = bool(0)
+        phone = fake.phone_number()
+        contact = Contact(fullname=fullname, email=email, ck_sent=ck_sent, phone=phone)            
+        contact.save()
+        channel.basic_publish(exchange='', routing_key='sending_emails_queue', body=f"{contact.id}")
+
+    connection.close()
     print("DONE.")
 
 
