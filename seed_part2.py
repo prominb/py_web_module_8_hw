6364314d4564e67577805933cdf1a4abea37dff6
Roman M <prominb@gmail.com>
2024-04-25 03:55:30 +0300

    edit seed-part2 e008

--- seed_part2.py
+++ seed_part2.py
@@ -7,10 +7,13 @@ fake = Faker()
 
 def fill_data():
     Contact.drop_collection()
-    for i in range(5):
+    for _ in range(5):
         record = Contact()
         record.fullname = fake.name()
         record.email = fake.email()
         record.ck_sent = bool(0)
         record.phone = fake.phone_number()
         record.save()
+
+# if __name__ == '__main__':
+#     fill_data()
