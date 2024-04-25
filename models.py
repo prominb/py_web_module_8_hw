e0e57a2bb5762e49c45e231ee54469271f38c75a
Roman M <prominb@gmail.com>
2024-04-25 03:21:37 +0300

    edit Models 2time CONNECT method

--- models.py
+++ models.py
@@ -1,18 +1,3 @@
-# import configparser
-# from bson import json_util
-# from mongoengine import connect, Document, StringField, ReferenceField, ListField, CASCADE
-
-
-# config = configparser.ConfigParser()
-# config.read('config.ini')
-
-# mongo_user = config.get('DB', 'user')
-# mongodb_pass = config.get('DB', 'pass')
-# db_name = config.get('DB', 'db_name')
-# domain = config.get('DB', 'domain')
-
-# # connect to cluster on AtlasDB with connection string
-# connect(host=f"""mongodb+srv://{mongo_user}:{mongodb_pass}@{domain}/{db_name}?retryWrites=true&w=majority""", ssl=True)
 from bson import json_util
 
 from mongoengine import Document, StringField, ReferenceField, ListField, CASCADE
